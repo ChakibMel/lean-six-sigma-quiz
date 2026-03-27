19f4fec02c920ad30b49b9b1044e6b48eed22293
Chakib Melaika <chakib.melaika@aptushealth.com>
2026-03-27 15:05:45 +0100

    fix blank page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/ChakibMel/lean-six-sigma-quiz.git', // Replace with your actual GitHub repository name
+  base: '/lean-six-sigma-quiz/', // Replace with your actual GitHub repository name
 })
 
